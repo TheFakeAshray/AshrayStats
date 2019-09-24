128b642b38a695f550776cd2c913a5b053ef496a
Ashray Shetty <Ashray.Shetty@empired.com>
2019-09-24 14:48:58 +1000

    updated lol api to get encryptedsummonerid dynamically

--- server.js
+++ server.js
@@ -2,16 +2,16 @@
 var express = require("express");
 var app = express();
 var HTMLParser = require("fast-html-parser");
-require('dotenv').config()
+require("dotenv").config();
 
 app.get("/", function(req, res) {
-    res.sendfile("index.html", { root: __dirname + "/public" });
+	res.sendfile("index.html", { root: __dirname + "/public" });
 });
 //Need this for Steam/LoL Api's
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
 });
 var request = require("request");
 
@@ -22,92 +22,98 @@ var request = require("request");
 //     headers: {'Content-Type' : 'application/json'}
 // }
 function apiCall(options, callback) {
-    request(options, function(error, httpResponse, httpBody) {
-        if (httpResponse.statusCode == "200") {
-            console.log("apiCall Success");
-            return callback(httpResponse, httpBody);
-        } else {
-            console.log("error:", httpResponse.statusCode);
-            return callback(httpResponse, httpBody);
-        }
-    });
+	request(options, function(error, httpResponse, httpBody) {
+		if (httpResponse.statusCode == "200") {
+			console.log("apiCall Success");
+			return callback(httpResponse, httpBody);
+		} else {
+			console.log("error:", httpResponse.statusCode);
+			return callback(httpResponse, httpBody);
+		}
+	});
 }
 
 app.get("/steam/latestgames", function(httpRequest, httpResponse) {
-    // Create the Steam API URL. we want to use process.env.SteamKey
-    var options = {
-        url:
-            "http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=" +
-            process.env.SteamKey +
-            "&steamid=76561198078986044&format=json",
-        method: "GET"
-    };
-    apiCall(options, function(response, body) {
-        httpResponse.send(body);
-    });
+	// Create the Steam API URL. we want to use process.env.SteamKey
+	var options = {
+		url:
+			"http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=" +
+			process.env.SteamKey +
+			"&steamid=76561198078986044&format=json",
+		method: "GET"
+	};
+	apiCall(options, function(response, body) {
+		httpResponse.send(body);
+	});
 });
 
 app.get("/lol/rank", function(httpRequest, httpResponse) {
-    console.log(process.env.LolKey);
-    var options = {
-        url: "https://oc1.api.riotgames.com/lol/league/v4/entries/by-summoner/HALmw2koFqag9gOJqxkuGRqNaKMg_HA4bqgM2tAze3Vgbw?api_key=" + process.env.LolKey,
-        method: "GET"
-    };
-    apiCall(options, function(response, body) {
-        httpResponse.send(body);
-    });
+	var summonerOptions = {
+		url: "https://oc1.api.riotgames.com/lol/summoner/v4/summoners/by-name/balnce?api_key=" + process.env.LolKey,
+		method: "GET"
+	};
+	apiCall(summonerOptions, function(response, body) {
+		const { id } = JSON.parse(body);
+		var options = {
+			url: `https://oc1.api.riotgames.com/lol/league/v4/entries/by-summoner/${id}?api_key=${process.env.LolKey}`,
+			method: "GET"
+		};
+		apiCall(options, function(response, body) {
+			httpResponse.send(body);
+		});
+	});
 });
 
 app.get("/cinebuzz/getPastBookings", function(httpRequest, httpResponse) {
-    console.log("hey");
-    // apiCall(httpResponse, url,header)
-    var loginOptions = {
-        url: "https://www.eventcinemas.co.nz/cinebuzz/login?Username=dabalnce&Password=" + process.env.cineBuzz,
-        method: "POST"
-    };
-    apiCall(loginOptions, function(loginResponse, loginBody) {
-        var cookie = loginResponse.headers["set-cookie"];
-        var bookingsOptions = {
-            url: "https://www.eventcinemas.co.nz/cinebuzz/getpastbookings",
-            method: "POST",
-            headers: { Cookie: cookie }
-        };
-        apiCall(bookingsOptions, function(bookingsResponse, bookingsBody) {
-            //Lets parse this ugly HTML into some Mighty Fine JSON
-            var htmlResult = HTMLParser.parse(bookingsBody);
-            html = htmlResult.querySelectorAll('.\\"booking\\"');
-            var jsonBookings = [];
-            var previousJsonEntry;
-            for (var x in html) {
-                var booking = html[x]
-                    .removeWhitespace()
-                    .text.replace(/(?:\\[rn])+/g, ",")
-                    .slice(1, -1);
-                var entry = booking.split(",");
-                var jsonEntry = {
-                    Title: entry[0],
-                    Date: entry[1],
-                    Time: entry[2].slice(1, 9),
-                    Location: entry[2].slice(13),
-                    Points: entry[3]
-                };
-                // Eliminating unwanted results
-                // Firstly, duplicates (Where I booked tickets for multiple people)
-                if (
-                    jsonEntry.Title != previousJsonEntry &&
-                    //If it was at WestCity, it was someone using my card (glitch in their system)
-                    !jsonEntry.Location.includes("Westcity") &&
-                    //If location is empty (date is headoffice), it was because I was granted points, because of someone using my card.
-                    !(jsonEntry.Location == "")
-                ) {
-                    previousJsonEntry = jsonEntry.Title;
-                    jsonBookings.push(jsonEntry);
-                }
-            }
-            // console.log(jsonBookings);
-            httpResponse.send(jsonBookings);
-        });
-    });
+	console.log("hey");
+	// apiCall(httpResponse, url,header)
+	var loginOptions = {
+		url: "https://www.eventcinemas.co.nz/cinebuzz/login?Username=dabalnce&Password=" + process.env.cineBuzz,
+		method: "POST"
+	};
+	apiCall(loginOptions, function(loginResponse, loginBody) {
+		var cookie = loginResponse.headers["set-cookie"];
+		var bookingsOptions = {
+			url: "https://www.eventcinemas.co.nz/cinebuzz/getpastbookings",
+			method: "POST",
+			headers: { Cookie: cookie }
+		};
+		apiCall(bookingsOptions, function(bookingsResponse, bookingsBody) {
+			//Lets parse this ugly HTML into some Mighty Fine JSON
+			var htmlResult = HTMLParser.parse(bookingsBody);
+			html = htmlResult.querySelectorAll('.\\"booking\\"');
+			var jsonBookings = [];
+			var previousJsonEntry;
+			for (var x in html) {
+				var booking = html[x]
+					.removeWhitespace()
+					.text.replace(/(?:\\[rn])+/g, ",")
+					.slice(1, -1);
+				var entry = booking.split(",");
+				var jsonEntry = {
+					Title: entry[0],
+					Date: entry[1],
+					Time: entry[2].slice(1, 9),
+					Location: entry[2].slice(13),
+					Points: entry[3]
+				};
+				// Eliminating unwanted results
+				// Firstly, duplicates (Where I booked tickets for multiple people)
+				if (
+					jsonEntry.Title != previousJsonEntry &&
+					//If it was at WestCity, it was someone using my card (glitch in their system)
+					!jsonEntry.Location.includes("Westcity") &&
+					//If location is empty (date is headoffice), it was because I was granted points, because of someone using my card.
+					!(jsonEntry.Location == "")
+				) {
+					previousJsonEntry = jsonEntry.Title;
+					jsonBookings.push(jsonEntry);
+				}
+			}
+			// console.log(jsonBookings);
+			httpResponse.send(jsonBookings);
+		});
+	});
 });
 
 app.use("/", express.static("public"));
