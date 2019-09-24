ba8a1d68bd4f70e5cf20390b5f51a109e8320bd2
Ashray Shetty <Ashray.Shetty@empired.com>
2019-09-24 17:33:11 +1000

    *space*

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.get('/lol/rank', (httpRequest, httpResponse) => {
 app.get('/cinebuzz/getPastBookings', (httpRequest, httpResponse) => {
 	// apiCall(httpResponse, url,header)
 	const loginOptions = {
-		url: `https://www.eventcinemas.co.nz/cinebuzz/login?Username=dabalnce&Password= ${process.env.cineBuzz}`,
+		url: `https://www.eventcinemas.co.nz/cinebuzz/login?Username=dabalnce&Password=${process.env.cineBuzz}`,
 		method: 'POST'
 	};
 	apiCall(loginOptions, (loginResponse, loginBody) => {
